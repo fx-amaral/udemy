04fa28d779c9ab1c79f36c01b68f0093729b39b8
fx-amaral <fx_lucris@yahoo.com.br>
2026-04-28 10:39:44 -0300

    ex primeiro digito cpf

--- aula56splitstrip.py
+++ aula56splitstrip.py
@@ -10,7 +10,7 @@ lista_frases = []
 for i, frase in enumerate(lista_frases_cruas):
     lista_frases.append(lista_frases_cruas[i].strip())
 
-# print(lista_frases_cruas)
-# print(lista_frases)
+print(lista_frases_cruas)
+print(lista_frases)
 frases_unidas = ', '.join(lista_frases)
 print(frases_unidas)
