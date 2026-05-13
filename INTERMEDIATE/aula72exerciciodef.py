ef1ed788b61272bcd4fe5cbc684bdb9936f9cfe4
fx-amaral <fx_lucris@yahoo.com.br>
2026-05-13 10:17:51 -0300

    mais um pouco de funções

---
+++ INTERMEDIATE/aula72exerciciodef.py
@@ -0,0 +1,37 @@
+# Exercícios com funções
+
+# Crie uma função que multiplica todos os argumentos
+# não nomeados recebidos
+# Retorne o total para uma variável e mostre o valor
+# da variável.
+
+def mult (*args):
+    total = 1
+    for numero in args:
+        total *= numero
+    return total
+
+def par_impar(x):
+    x = x%2
+    if x == 0:
+        x = 'par'
+    else:
+        x = 'impar' 
+    return x    
+
+seq = 5,10,3,4,8
+prod = mult(*seq)
+print(prod)
+
+#num = int(input())
+print(par_impar(10))
+
+#versão par_impar do professor
+
+def par_impar (numero):
+    multiplo_de_dois = numero % 2 == 0
+    if multiplo_de_dois:
+        return f'{numero} é par'
+    return f'{numero} é impar'
+
+print(par_impar(16))
