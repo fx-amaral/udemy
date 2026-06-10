3d5015d989129c5e3438903777be913299953a7d
fx-amaral <fx_lucris@yahoo.com.br>
2026-06-10 09:22:18 -0300

    list comprehension

--- BASIC/nitenslista.py
+++ BASIC/nitenslista.py
@@ -2,7 +2,7 @@ def n_itens (lista):
     if lista == []:
         return 0
     else:
-        return 1 + n_itens(lista[1:])
+        return len(lista) #1 + n_itens(lista[1:])
     
 
 lista = [1, 7, 6 , 4, 0, 9, 0, 25, 10]
--- aula84listcompreension.py
+++ aula84listcompreension.py
@@ -3,13 +3,29 @@
 # a partir de iteráveis.
 print(tuple(range(29)))
 print()
-lista = []
-for numero in range(10):
+lista = []                     
+for numero in range(10): #criando lista com for
     lista.append(numero)
 print(lista)
 
 lista = [
     numero * 2
-    for numero in range(10)
+    for numero in lista   #range(10) #list comprehension
 ]
 print(lista)
+
+# Mapeamento de dados em list comprehension
+produtos = [
+    {'nome': 'p1', 'preco': 20, },
+    {'nome': 'p2', 'preco': 10, },
+    {'nome': 'p3', 'preco': 30, },
+] 
+novos_produtos = [
+    {**produto, 'preco': produto['preco'] * 1.05}
+    if produto['preco'] > 20 else {**produto}
+    for produto in produtos
+]
+print()
+print('produtos', *produtos, sep='\n')
+print()
+print('produtos novos', *novos_produtos, sep='\n')
