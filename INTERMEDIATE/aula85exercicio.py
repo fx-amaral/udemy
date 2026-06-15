594ca93c64a5da5e530897cac6cee73fa76bdd82
fx-amaral <fx_lucris@yahoo.com.br>
2026-06-15 09:51:51 -0300

    organizando algumas aulas

---
+++ INTERMEDIATE/aula85exercicio.py
@@ -0,0 +1,6 @@
+#colocar a última letra de cada nome em maiúscula
+
+nomes = ['luiz','maria','helena','joana','felipe']
+novos_nomes = [f'{nome[:-1].lower()}{nome[-1].upper()}' 
+               for nome in nomes]
+print(novos_nomes)
